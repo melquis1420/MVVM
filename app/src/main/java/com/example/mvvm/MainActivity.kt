c7115a1ccb45d05f1fcf8c0179eb959ac8419db6
melquis1420 <melqui1420@gmail.com>
2021-04-27 11:15:11 -0300

    Change in text

--- app/src/main/java/com/example/mvvm/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.mvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainViewModel
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel=ViewModelProvider(this).get(MainViewModel::class.java)
+        viewModel.textWelcome.observe(this, Observer {
+            textWelcome.text=it
+
+        })
+
+
+
+
+
     }
 }
